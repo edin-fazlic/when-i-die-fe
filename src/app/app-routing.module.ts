32af4cd211443643e5f6668694715d007b280c10
dootsquared <127309735+dootsquared@users.noreply.github.com>
2024-05-18 11:56:34 +0200

    add routes (#2)
    Co-authored-by: Darin <anic.darin@gmail.com>
    Co-authored-by: ilma-s <104169760+ilma-s@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import {Route} from "./constants/routes";
+import {AppComponent} from "./app.component";
 import { LoginComponent } from './features/login/login.component';
 import { RegistrationComponent } from './features/registration/registration.component';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: Route.login,
     component: LoginComponent
   },
   {
-    path: 'register',
+    path: Route.register,
     component: RegistrationComponent
   },
+  {
+    path: Route.settings,
+    component: AppComponent
+  },
+  {
+    path: Route.home,
+    component: AppComponent
+  },
+  {
+    path: Route.tp,
+    children: [
+      {
+        path: '',
+        component: AppComponent
+      },
+      {
+        path: Route.tpDetails,
+        component: AppComponent
+      },
+      {
+        path: Route.tpEdit,
+        component: AppComponent
+      },
+    ]
+  },
+  {
+    path: Route.messages,
+    children: [
+      {
+        path: '',
+        component: AppComponent
+      },
+      {
+        path: Route.messageDetails,
+        component: AppComponent
+      },
+    ]
+  },
+  {
+    path: Route.theyTrustMe,
+    component: AppComponent
+  }
 ];
 
 @NgModule({
