b8f577876bcf3d3675a513c7dde89c50e0e12383
Darin <anic.darin@gmail.com>
2024-05-18 11:53:05 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,60 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Route} from "./constants/routes";
+import {AppComponent} from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: Route.login,
+    component: AppComponent
+  },
+  {
+    path: Route.register,
+    component: AppComponent
+  },
+  {
+    path: Route.settings,
+    component: AppComponent
+  },
+  {
+    path: Route.home,
+    component: AppComponent
+  },
+  {
+    path: Route.tp,
+    children: [
+      {
+        path: '',
+        component: AppComponent
+      },
+      {
+        path: Route.tpDetails,
+        component: AppComponent
+      },
+      {
+        path: Route.tpEdit,
+        component: AppComponent
+      },
+    ]
+  },
+  {
+    path: Route.messages,
+    children: [
+      {
+        path: '',
+        component: AppComponent
+      },
+      {
+        path: Route.messageDetails,
+        component: AppComponent
+      },
+    ]
+  },
+  {
+    path: Route.theyTrustMe,
+    component: AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/constants/routes.ts
@@ -0,0 +1,12 @@
+export enum Route {
+  login= 'login',
+  register= 'register',
+  settings= 'settings',
+  home= 'home',
+  messages= 'messages',
+  messageDetails= 'messageDetails',
+  tp= 'tp',
+  tpDetails= 'tpDetails',
+  tpEdit= 'tpEdit',
+  theyTrustMe= 'theyTrustMe'
+}
